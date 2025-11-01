d534354e61cc2012e89a34a2a152c7a34daf0d18
Selina Tung <hippogif04@gmail.com>
2025-11-01 01:20:44 +0000

    a whole lot more constants

--- wowee.js
+++ wowee.js
@@ -15,3 +15,12 @@ const nextBtn = document.getElementById('nextBtn');
 const loopBtn = document.getElementById('loopBtn');
 const trackName = document.getElementById('trackName');
 const progress = document.getElementById('progress');
+const progressBar = document.getElementById('progressBar');
+const currentTime = document.getElementById('currentTime');
+const duration = document.getElementById('duration');
+const reel1 = document.getElementById('reel1');
+const reel2 = document.getElementById('reel2');
+const volumeSlider = document.getElementById('volumeSlider');
+const volumeLevel = document.getElementById('volumeLevel');
+
+audio.volume = 0.7;
