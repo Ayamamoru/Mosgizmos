690140ae0dc971f8bd2558dce2eef565b7de22ac
Selina Tung <hippogif04@gmail.com>
2025-11-01 01:16:44 +0000

    a pile of constants...

--- wowee.js
+++ wowee.js
@@ -3,3 +3,15 @@ const tracks = [
     { name: 'Wall', file: 'Wall.mp3' },
     { name: 'Madeline', file: 'Madeline (fr.Loupe).mp3' }
 ];
+
+let currentTrack=0;
+let isPlaying=false;
+let isLooping=false;
+
+const audio = new Audio(tracks[0].file);
+const playBtn = document.getElementById('playBtn');
+const prevBtn = document.getElementById('prev.Btn');
+const nextBtn = document.getElementById('nextBtn');
+const loopBtn = document.getElementById('loopBtn');
+const trackName = document.getElementById('trackName');
+const progress = document.getElementById('progress');
